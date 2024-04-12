d20604b3ca29575c355e69966db26010bdf343d5
Hidaya Wetsakhulia <hidayawetsa@gmail.com>
2024-04-12 05:09:34 +0300

    almost done

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //fetching attempt
-function fetchQuotes(){
-    return fetch("https://type.fit/api/quotes")
-    .then(res =>res.json())
-    .then(displayQuotes)
-}; 
+// function fetchQuotes(){
+//     return fetch("https://type.fit/api/quotes")
+//     .then(res =>res.json())
+//     .then(displayQuotes)
+// }; 
 
 //selecting elements for the DOM
 const quotes = document.getElementById('quote');
@@ -14,22 +14,23 @@ const gallery = document.getElementById("images");
 const videoStudio = document.getElementById("vida");
 const audioBox = document.getElementById("audio");
 const audioList = document.getElementById("li");
-
-//function that renders quotes
-function displayQuotes(words){
-    // quoteInfo.textContent = words.text;
-    words.forEach(word => {
-        const lis = document.createElement('li');
-        lis.innerText = word.text;
-        listOfQuotes.appendChild(lis);
-        
-    });
-};
-fetchQuotes()
 const quote = document.querySelector('.quote');
 const writer = document.querySelector('.writer');
-const button = document.getElementById("qbtn")
-  
+const button = document.getElementById("qbtn");
+const darkmode = document.getElementById("toggle")
+
+//function that renders quotes
+// function displayQuotes(words){
+//     // quoteInfo.textContent = words.text;
+//     words.forEach(word => {
+//         const lis = document.createElement('li');
+//         lis.innerText = word.text;
+//         listOfQuotes.appendChild(lis);
+        
+//     });
+// // };
+// fetchQuotes()
+ //function that allows user to generate random quotes  
 button.addEventListener("click", getQuote);
 function getQuote(){
     fetch("https://api.quotable.io/random")
@@ -39,14 +40,10 @@ function getQuote(){
         writer.innerHTML =`${data.author}`;
     })
 }
-
-// function getImages(){
-//     fetch("jg4isq57kpRbfNisjsgeEl7GoQ69HBOoRfu3Br7tLtAgtwmqT3X49DWr")
-//     .then(res => res.json())
-//     .then(datas =>{
-       
-       
-//         gallery.innerHTML  = `${datas.url}`;
-//     })
-// };
+ //function that enables user to toggle between dark and light mode
+function darkMode(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+};
+   darkmode.addEventListener("click",darkMode)
 
